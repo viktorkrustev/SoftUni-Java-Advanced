911113bd2b534a9ee7bf1a173a055832bc1c55d8
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-17 22:09:30 +0300

    Functional Programming

---
+++ Functional Programming - Exercise/CustomMinFunction_03.java
@@ -0,0 +1,20 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CustomMinFunction_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> getMinNum = list -> Collections.min(list);
+        System.out.println(getMinNum.apply(numbers));
+    }
+}

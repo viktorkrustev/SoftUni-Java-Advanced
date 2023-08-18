66851adb52efe299c06197b16431db7fbcda1856
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-18 20:40:41 +0300

    Exercise

---
+++ Defining classes - Lab/CarInfo_01/Car.java
@@ -0,0 +1,42 @@
+package DefiningClassesLab._01;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int hp;
+
+    public Car(String brand, String model, int hp){
+        this.brand = brand;
+        this.model = model;
+        this.hp = hp;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.",this.getBrand(), this.getModel(), this.getHp());
+    }
+}

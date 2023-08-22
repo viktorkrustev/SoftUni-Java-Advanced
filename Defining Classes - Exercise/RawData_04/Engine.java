93d3be4d2faad9bc987fb5a490973c41548feb05
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-22 23:21:43 +0300

    Exercise

---
+++ Defining Classes - Exercise/RawData_04/Engine.java
@@ -0,0 +1,15 @@
+package DefiningClassesExercises.ex_04;
+
+public class Engine {
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+}

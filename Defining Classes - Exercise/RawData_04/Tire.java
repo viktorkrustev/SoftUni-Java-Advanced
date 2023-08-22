1fe7c3ff847db2c6fccfd54a43917488ce5332ef
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-22 23:22:16 +0300

    Exercise

---
+++ Defining Classes - Exercise/RawData_04/Tire.java
@@ -0,0 +1,15 @@
+package DefiningClassesExercises.ex_04;
+
+public class Tire {
+    private double pressure;
+    private int age;
+
+    public Tire(double pressure, int age) {
+        this.pressure = pressure;
+        this.age = age;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+}

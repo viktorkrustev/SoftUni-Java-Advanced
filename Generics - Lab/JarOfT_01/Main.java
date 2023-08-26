3a0b60fcd9c426545f892b1975f327bdf04fb3ec
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-26 20:24:19 +0300

    Exercise

---
+++ Generics - Lab/JarOfT_01/Main.java
@@ -0,0 +1,10 @@
+package GenericsLab.JarOfT_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}

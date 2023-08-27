a5490923b2fd166fb0bb05ceb3b2b9e7faeb550e
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-27 19:43:29 +0300

    Exercise

---
+++ Generics - Exercises/GenericBoxOfInteger_02/Box.java
@@ -0,0 +1,24 @@
+package GenericsExercises.GenericBoxOfInteger_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T> {
+    List<T> list;
+
+    public Box(List<T> list) {
+        this.list = new ArrayList<>();
+    }
+    public void add(T el){
+        this.list.add(el);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T element: list){
+            sb.append(element.getClass().getName()).append(": ").append(element).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+}

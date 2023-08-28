6df7cec6365c3319a291ae4eade16fac466f45a0
viktorkrustev <viktorkrustev03@abv.bg>
2023-08-28 22:15:46 +0300

    Exercise

---
+++ Generics - Exercises/GenericSwapMethodStrings_03/Main.java
@@ -0,0 +1,20 @@
+package GenericsExercises.GenericSwapMethodStrings_03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            box.add(input);
+        }
+        int firstIndex = scanner.nextInt();
+        int secondIndex = scanner.nextInt();
+        box.swap(firstIndex, secondIndex);
+        System.out.println(box.toString());
+
+    }
+}
